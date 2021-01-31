2776f6fb35bf91dbb8209c6dacf1935b30bb1649
Preetesh Verma <verma.preetesh21@gmail.com>
2021-01-31 15:43:34 +0530

    adding json serializability in flask

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import numpy as np
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,jsonify, make_response
 import pickle
 from sklearn.preprocessing import LabelEncoder
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -14,15 +15,18 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    print(request)
-    int_features = [float(x) for x in request.form.values()]
-    #print(int_features)
+    print (request.is_json)
+    content = request.get_json()
+    print (content.values())
+    #return jsonify(content)
+    int_features = [float(x) for x in content.values()]
+    print(int_features)
     int_features=np.array(int_features)
     int_features.shape=(1,11)
     print(int_features)
     prediction = model.predict(int_features)
-    return render_template('index.html', prediction_text='Bank Loan Approval Prediction is prediction{}'.format(prediction))
-
+    print(prediction,prediction[0])
+    return(jsonify(predict=int(prediction[0])))
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(host="localhost", port=8000,debug = True)
